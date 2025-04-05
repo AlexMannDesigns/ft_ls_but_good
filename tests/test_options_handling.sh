e2fb79fd6afb5b3f133821a9d97de77ceacd2cfc
Alexander Mann <alex.mann@wolt.com>
2025-04-05 14:58:01 +0300

    add complex tests

--- tests/test_options_handling.sh
+++ tests/test_options_handling.sh
@@ -7,8 +7,11 @@
 # Param 2: expected output
 function options_tester () {
     # rename params
-    ARGS_ARRAY=$1
+    ARGS_STRING=$1
     EXPECTED_OUTPUT=$2
+
+    # convert args to array
+    ARGS_ARRAY=($ARGS_STRING)
 
     # set up temp files
     FT_LS_TEST_OPTIONS_OUTPUT=$(mktemp)
@@ -17,17 +20,15 @@ function options_tester () {
     # run ./ft_ls --TEST + commandline args > temp_file
     ./ft_ls --TEST "${ARGS_ARRAY[@]}" > "$FT_LS_TEST_OPTIONS_OUTPUT"
 
-    # cat "$FT_LS_TEST_OPTIONS_OUTPUT"
-
     # echo the expected output into another temp file
     echo "$EXPECTED_OUTPUT" > "$EXPECTED_TEST_OPTIONS_OUTPUT"
-
+    
     # diff the two temp files, outputting any errors into a test results file
     if diff -u "$EXPECTED_TEST_OPTIONS_OUTPUT" "$FT_LS_TEST_OPTIONS_OUTPUT" > /dev/null; then
         echo "$TEST_PASS"
     else
         echo "$TEST_FAIL"
-        echo "----- ./ft_ls" "${ARGS_ARRAY[@]}" "-----" >> "$ERROR_LOG" 
+        echo "//----- ./ft_ls" "${ARGS_ARRAY[@]}" "-----//" >> "$ERROR_LOG" 
         diff -u "$EXPECTED_TEST_OPTIONS_OUTPUT" "$FT_LS_TEST_OPTIONS_OUTPUT" >> "$ERROR_LOG"
         echo "" >> "$ERROR_LOG"
     fi
@@ -39,10 +40,11 @@ function options_tester () {
 # This test covers the most basic option handling (e.g. ./ft_ls -l)
 function test_basic_options () {
     # echo basic tests start
-    echo "/----- BASIC TESTS -----/"
+    echo "//----- BASIC OPTIONS TESTS -----//"
 
     # Test cases array
     TEST_CASES=(
+        ""
         "-l"
         "-m"
         "-1"
@@ -55,7 +57,8 @@ function test_basic_options () {
 
     # Expected output array
     EXPECTED_OUTPUT_ARRAY=(
-        "display = long\nsort = lexicographical\nrecursive = 0\nreversed = 0\nall = 0\nargv_idx = 4"
+        "display = columns\nsort = lexicographical\nrecursive = 0\nreversed = 0\nall = 0\nargv_idx = 2"
+        "display = long\nsort = lexicographical\nrecursive = 0\nreversed = 0\nall = 0\nargv_idx = 3"
         "display = comma\nsort = lexicographical\nrecursive = 0\nreversed = 0\nall = 0\nargv_idx = 3"
         "display = one\nsort = lexicographical\nrecursive = 0\nreversed = 0\nall = 0\nargv_idx = 3"
         "display = columns\nsort = time\nrecursive = 0\nreversed = 0\nall = 0\nargv_idx = 3"
@@ -72,22 +75,50 @@ function test_basic_options () {
     # call options_tester() with each test case in a loop, echoing each test case
     for (( i=0; i<$NUM_TESTS; i++ )); do
         OPTION_STRING="${TEST_CASES[$i]}"
-        OPTION_ARRAY=($OPTION_STRING)
         EXPECTED_OUTPUT="${EXPECTED_OUTPUT_ARRAY[$i]}"
 
         echo "test case: [ ./ft_ls" $OPTION_STRING " ]"
-        options_tester "$OPTION_ARRAY" "$EXPECTED_OUTPUT"
+        options_tester "$OPTION_STRING" "$EXPECTED_OUTPUT"
     done
+    echo ""
 }
 
 # This test covers some more convoluted (but valid) option sequences (e.g. ./ft_ls -lR -ta -r)
 function test_complex_options () {
-    echo NOT YET IMPLEMENTED
-}
+    echo "//----- COMPLEX OPTIONS TESTS -----//"
+    
+    # Test cases array
+    TEST_CASES=(
+        "-la"
+        "-l -a"
+        "-l -r -t"
+        "-a -rS"
+        "-lR -ta -r"
+        "-lRtarm"
+    )
 
-# i.e. ./ftls
-function test_no_options () {
-    echo NOT YET IMPLEMENTED
+    # Expected output array
+    EXPECTED_OUTPUT_ARRAY=(
+        "display = long\nsort = lexicographical\nrecursive = 0\nreversed = 0\nall = 1\nargv_idx = 3"
+        "display = long\nsort = lexicographical\nrecursive = 0\nreversed = 0\nall = 1\nargv_idx = 4"
+        "display = long\nsort = time\nrecursive = 0\nreversed = 1\nall = 0\nargv_idx = 5"
+        "display = columns\nsort = size\nrecursive = 0\nreversed = 1\nall = 1\nargv_idx = 4"
+        "display = long\nsort = time\nrecursive = 1\nreversed = 1\nall = 1\nargv_idx = 5"
+        "display = comma\nsort = time\nrecursive = 1\nreversed = 1\nall = 1\nargv_idx = 3"
+    )
+    # Number of tests
+    NUM_TESTS=${#TEST_CASES[@]}
+
+    # Run tests
+    # call options_tester() with each test case in a loop, echoing each test case
+    for (( i=0; i<$NUM_TESTS; i++ )); do
+        OPTION_STRING="${TEST_CASES[$i]}"
+        EXPECTED_OUTPUT="${EXPECTED_OUTPUT_ARRAY[$i]}"
+
+        echo "test case: [ ./ft_ls" $OPTION_STRING " ]"
+        options_tester "$OPTION_STRING" "$EXPECTED_OUTPUT"
+    done
+    echo ""
 }
 
 # repeating an option should not change anything
@@ -110,13 +141,18 @@ function test_option_errors () {
 # Test Script starts here
 
 # global vars
-TEST_PASS="PASS"
-TEST_FAIL="FAIL"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+TEST_PASS="${GREEN}PASS${NC} ✅"
+TEST_FAIL="${RED}FAIL${NC} ❌"
 
 # setup error log file
 ERROR_LOG="test_errors.log"
 > "$ERROR_LOG"
 
 test_basic_options
+test_complex_options
 
 
