0a98f4d6b8e895adf07869799b7965c3c87694df
Alexander Mann <alex.mann@wolt.com>
2025-04-24 20:06:40 +0300

    add group column

--- src/print/print_control.c
+++ src/print/print_control.c
@@ -2,6 +2,7 @@
 #include "libft.h"
 #include <sys/types.h>
 #include <pwd.h>
+#include <grp.h>
 #include <uuid/uuid.h>
 
 /*
@@ -27,7 +28,7 @@ static void zero_field_widths(t_print *print)
     print->size_width = 0;
 }
 
-t_known_usr_grp add_node_to_known_usr_list(t_list **list, t_file_info file_info)
+t_known_usr_grp add_node_to_known_usr_list(t_list **known_usr_list, t_file_info file_info)
 {
     t_known_usr_grp usr_info;
     t_list          *new_node;
@@ -45,20 +46,45 @@ t_known_usr_grp add_node_to_known_usr_list(t_list **list, t_file_info file_info)
     new_node = ft_lstnew((void *) &usr_info, sizeof(t_known_usr_grp));
     if (!new_node)
         print_malloc_error_and_exit();
-    if (!*list)
-        *list = new_node;
+    if (!*known_usr_list)
+        *known_usr_list = new_node;
     else
-        ft_lstadd_back(list, new_node);
+        ft_lstadd_back(known_usr_list, new_node);
     return (usr_info);
 }
 
-void    check_known_usr(t_ls *state, t_file_info *current)
+t_known_usr_grp add_node_to_known_grp_list(t_list **known_grp_list, t_file_info file_info)
+{
+    t_known_usr_grp grp_info;
+    t_list          *new_node;
+	struct group    *grp;
+
+    ft_bzero((void *) &grp_info, sizeof(t_known_usr_grp));
+	grp = getgrgid(file_info.sys_file_info.st_gid);
+    if (!grp)
+        exit(EXIT_FAILURE);  // create error handler
+    grp_info.usr_grp_name_str = ft_strdup(grp->gr_name);
+    if (!(grp_info.usr_grp_name_str))
+        print_malloc_error_and_exit();
+    grp_info.usr_grp_name_len = ft_strlen(grp_info.usr_grp_name_str);
+    grp_info.usr_grp_id = file_info.sys_file_info.st_gid;
+    new_node = ft_lstnew((void *) &grp_info, sizeof(t_known_usr_grp));
+    if (!new_node)
+        print_malloc_error_and_exit();
+    if (!*known_grp_list)
+        *known_grp_list = new_node;
+    else
+        ft_lstadd_back(known_grp_list, new_node);
+    return (grp_info);
+}
+
+void    check_known_usr(t_list **known_usr_list, t_file_info *current)
 {
     t_list          *iter;
     t_known_usr_grp *current_usr;
     t_known_usr_grp new_usr_info;
 
-    iter = state->known_usr;
+    iter = *known_usr_list;
     while (iter)
     {
         current_usr = (t_known_usr_grp *) iter->content;
@@ -70,9 +96,33 @@ void    check_known_usr(t_ls *state, t_file_info *current)
         }
         iter = iter->next;
     }
-    new_usr_info = add_node_to_known_usr_list(&(state->known_usr), *current);
+    new_usr_info = add_node_to_known_usr_list(known_usr_list, *current);
     current->user_id_str = new_usr_info.usr_grp_name_str;
     current->user_id_str_len = new_usr_info.usr_grp_name_len;
+    return ;
+}
+
+void    check_known_grp(t_list **known_grp_list, t_file_info *current)
+{
+    t_list          *iter;
+    t_known_usr_grp *current_usr;
+    t_known_usr_grp new_usr_info;
+
+    iter = *known_grp_list;
+    while (iter)
+    {
+        current_usr = (t_known_usr_grp *) iter->content;
+        if (current_usr->usr_grp_id == current->sys_file_info.st_gid)
+        {
+            current->group_id_str = current_usr->usr_grp_name_str;
+            current->group_id_str_len = current_usr->usr_grp_name_len;
+            return ;
+        }
+        iter = iter->next;
+    }
+    new_usr_info = add_node_to_known_grp_list(known_grp_list, *current);
+    current->group_id_str = new_usr_info.usr_grp_name_str;
+    current->group_id_str_len = new_usr_info.usr_grp_name_len;
     return ;
 }
 
@@ -87,7 +137,8 @@ void    get_usr_and_grp(t_ls *state, t_list *files)
     while (iter)
     {
         current = (t_file_info *) iter->content;
-        check_known_usr(state, current);
+        check_known_usr(&(state->known_usr), current);
+        check_known_grp(&(state->known_grp), current);
         iter = iter->next;
     }
     return ;
@@ -103,13 +154,13 @@ void    get_field_widths(t_ls *state, t_list *files)
     t_file_info     *current;
     unsigned int    max_links;  // should be a 'max_lens' struct
     unsigned int    max_user;
-    // unsigned int    max_group;
+    unsigned int    max_group;
 
     zero_field_widths(&(state->print));
     // bzero max lens struct
     max_links = 0;
     max_user = 0;
-    // max_group = 0;
+    max_group = 0;
     iter = files;
     while (iter)
     {
@@ -124,9 +175,8 @@ void    get_field_widths(t_ls *state, t_list *files)
                 max_links = current->sys_file_info.st_nlink;
             if (current->user_id_str_len > max_user)
                 max_user = current->user_id_str_len;
-            // get_group_name(current);
-            // if (current->group_len > max_group)
-            //     max_group = current->group_len;
+            if (current->group_id_str_len > max_group)
+                max_group = current->group_id_str_len;
 
             // do likewise for other values width is needed for
         }
@@ -135,6 +185,7 @@ void    get_field_widths(t_ls *state, t_list *files)
     // set field widths in helper
     state->print.link_width = get_num_len(max_links) + 2;
     state->print.user_width = max_user + 1;
+    state->print.group_width = max_group + 2;
     // consider setting predefined vals for column padding
     return ;
 }
